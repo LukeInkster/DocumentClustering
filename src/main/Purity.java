68a496c10531b581fb7ef10b1d1b43160539a0be
LukeInkster <luke.inkster@hotmail.com>
2016-05-02 18:19:10 +1200

    Added purity class

---
+++ src/main/Purity.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Purity {
+	private static Optional<String> mostCommonTopic(List<Article> articles){
+		List<String> allTopics = articles
+			.stream()
+			.flatMap(a -> a.topics.stream())
+			.collect(Collectors.toList());
+
+		return allTopics
+			.stream()
+			.max((x,y) ->
+				Collections.frequency(allTopics, x) -
+				Collections.frequency(allTopics, y));
+	}
+
+	/**
+	 * @return The proportion of articles that contain the most common topic in the cluster
+	 */
+	public static double of(List<Article> articles){
+		Optional<String> mostCommon = mostCommonTopic(articles);
+
+		if (!mostCommon.isPresent()) return 1;
+
+		return (double)articles.stream().filter(a -> a.topics.contains(mostCommon.get())).count() /
+				(double)articles.size();
+	}
+}
